faaf5e2b2342a1d792b583445fe37411a24a8a06
むずでょ <muzudho1@gmail.com>
2019-01-30 20:22:53 +0900

    とりあえず。

---
+++ cube2x2-book-reduce/BookRecord.cs
@@ -0,0 +1,52 @@
+﻿namespace Grayscale.Cube2x2BookReduce
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// 定跡の内容。
+    /// </summary>
+    public class BookRecord
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookRecord"/> class.
+        /// </summary>
+        /// <param name="previousBoardText">1つ前の盤面。</param>
+        /// <param name="handle">90°回転させた箇所。</param>
+        /// <param name="ply">初期局面から何手目か。</param>
+        public BookRecord(string previousBoardText, int handle, int ply)
+        {
+            this.PreviousBoardText = previousBoardText;
+            this.Handle = handle;
+            this.Ply = ply;
+        }
+
+        /// <summary>
+        /// Gets or sets 1つ前の盤面。
+        /// </summary>
+        public string PreviousBoardText { get; set; }
+
+        /// <summary>
+        /// Gets or sets 90°回転させた箇所。
+        /// </summary>
+        public int Handle { get; set; }
+
+        /// <summary>
+        /// Gets or sets 初期局面から何手目か。
+        /// </summary>
+        public int Ply { get; set; }
+
+        /// <summary>
+        /// 定跡出力。
+        /// </summary>
+        /// <returns>定跡文字列。</returns>
+        public string ToText()
+        {
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                "{0} {1} {2}",
+                this.PreviousBoardText,
+                this.Handle,
+                this.Ply);
+        }
+    }
+}
---
+++ cube2x2-book-reduce/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Grayscale.Cube2x2BookReduce
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+    using System.Globalization;
+
+    class Program
+    {
+        /// <summary>
+        /// 定跡。
+        /// </summary>
+        private static Dictionary<string, BookRecord> book;
+
+        public static void Main(string[] args)
+        {
+            book = new Dictionary<string, BookRecord>();
+
+            ReadBook();
+
+            // 上書き保存。
+            File.WriteAllText("./book.txt", ToBookText());
+        }
+
+        /// <summary>
+        /// 定跡読込。
+        /// </summary>
+        public static void ReadBook()
+        {
+            book.Clear();
+            if (File.Exists("./book.txt"))
+            {
+                foreach (var line in File.ReadAllLines("./book.txt"))
+                {
+                    var tokens = line.Split(' ');
+                    book.Add(tokens[0], new BookRecord(tokens[1], int.Parse(tokens[2]), int.Parse(tokens[3])));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 定跡全文。
+        /// </summary>
+        /// <returns>定跡。</returns>
+        public static string ToBookText()
+        {
+            var builder = new StringBuilder();
+            foreach (var record in book)
+            {
+                // 14手以下の定跡だけ残す。
+                if (record.Value.Ply < 15)
+                {
+                    builder.AppendLine(string.Format(
+                        CultureInfo.CurrentCulture,
+                        "{0} {1}",
+                        record.Key,
+                        record.Value.ToText()));
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
